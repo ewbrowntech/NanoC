8bd72866e1df0eb823f9bc4794e22a640fb7cf38
Paulatil <payink13@gmail.com>
2022-11-16 14:06:33 -0600

    [Paul Atilola] main.py file now updated to check before printing symbol table

--- src/main.py
+++ src/main.py
@@ -34,12 +34,13 @@ def main():
     parseTree = parse(tokens)  # Parse tokens into a grammatical parse tree
     if args.parseTree:
         print_parseTree(parseTree)
-    ast = astGen(parseTree)  # Transform parse tree into an abstract syntax tree
-    if args.ast:
-        print_ast(ast)
+    #ast = astGen(parseTree)  # Transform parse tree into an abstract syntax tree
+    #if args.ast:
+     #   print_ast(ast)
     symbolTable = generate_symbolTable(parseTree)  # Traverse parse tree to build a symbol table
-    if args.symbolTable:
-        print_symbolTable(symbolTable)
+    if symbolTable:
+        if args.symbolTable:
+            print_symbolTable(symbolTable)
 
 
 if __name__ == '__main__':
