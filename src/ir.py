e4e408c2ab6fe0a5333e43280b53e6b16901cfa4
Ethan Brown <ewbrowntech@gmail.com>
2022-11-29 14:00:13 -0600

    [Ethan] Rewrote ir.py to function with new AST approach

--- src/ir.py
+++ src/ir.py
@@ -3,12 +3,13 @@ ir.py
 
 @Author - Ethan Brown - ewb0020@auburn.edu
 
-@Version - 28 NOV 22
+@Version - 29 NOV 22
 
 Generates 3-Address Code Intermediate Representation (IR) from AST
 '''
 
 ir = ""
+
 def generate_ir(ast):
     for function in ast:
         unpack_function(ast, function)
@@ -16,77 +17,36 @@ def generate_ir(ast):
 
 def unpack_function(ast, function):
     global ir
-    function = ast[function]
-    for variable in function:   # Do top-level variable assignment last
-        if not type(function[variable]) == dict: continue
-        unpack_variable(function, variable)
-    for variable in function:
-        if type(function[variable]) == dict: continue
-        if variable == 'return':
-            add_returnCode(function['return'])
+    ops = ['+', '-']
+    instructions = ast[function]
+    for instruction in instructions:
+        instructionOps = []
+        for character in instruction:
+            if character in ops:
+                instructionOps.append(character)
+        if len(instructionOps) > 1: # IR is 3-address, so instructions with more than one op must be split
+            ir += split_instruction(instruction)
         else:
-            add_assignmentCode(function, variable)
+            ir += instruction + "\n"
 
-def unpack_variable(function, variable):
-    expression = function[variable]
-    unpack_expression(variable, expression)
+def split_instruction(instruction):
+    ops = ['+', '-']
+    identifier = instruction.split("=")[0].strip()
+    instructionOps = []
+    for character in instruction:
+        if character in ops:
+            instructionOps.append(character)  # Get every op in the instruction
 
-def unpack_expression(variable, expression):
-    global ir
-    # print(variable)
-    # print(expression)
-    operator, operands = list(expression.keys())[0], list(expression.values())[0]
-    operand1 = operands[0]
-    operand2 = operands[1]
-    # print(operator)
-    # print(operands)
-
-    maincode = variable + " = "
-    # print(maincode)
-
-    #operand1
-    if type(operand1) == dict:
-        maincode += str(list(operand1.keys())[0]) + " "
-        code = str(list(operand1.keys())[0]) + " = " + str(list(operand1.values())[0]) + "\n"
-        if code not in ir:
-            ir += code
-        # print(code)
-    else:
-        maincode += operand1 + " "
-
-    maincode += operator + " "
-
-    #operand2
-    if type(operand2) == dict:
-        maincode += str(list(operand2.keys())[0]) + " "
-        code = str(list(operand2.keys())[0]) + " = " + str(list(operand2.values())[0]) + "\n"
-        if code not in ir:
-            ir += code
-        # print(code)
-    else:
-        maincode += operand2 + "\n"
-
-    maincode += "\n"
-    if maincode not in ir:
-        ir += maincode
-    # print(maincode)
-    # print()
-
-def add_assignmentCode(function, variable):
-    global ir
-    code = str(variable) + " = " + str(function[variable]) + "\n"
-    if code not in ir:
-        ir += code
-
-def add_returnCode(returnExpression):
-    global ir
-    code = "return " + str(returnExpression) + "\n"
-    if code not in ir:
-        ir += code
+    expression = instruction.split("=")[1].strip()
+    instructionH1 = identifier + " = " + expression.split(instructionOps[1])[0].strip()
+    instructionH2 = identifier + " = " + identifier + " " + instructionOps[1] + " " + \
+                    expression.split(instructionOps[1])[1].strip()
+    modifiedInstruction = instructionH1 + "\n" + instructionH2 + "\n"
+    return modifiedInstruction
 
 class irException(Exception):
     pass
 
 def print_ir(ir):
     print("Intermediate Representation:")
-    print(str(ir) + "\n")
+    print(str(ir))
