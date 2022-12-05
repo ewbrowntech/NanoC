0bb7488caa6a343ff2f65b1c59c1fb194631d700
Paulatil <payink13@gmail.com>
2022-12-05 04:19:11 -0600

    [Paul Atilola] the nasm.py generates the x86 assembly code from the optimized IR. updated main/py accordingly

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from compilation.ast import astGen, print_ast
 from compilation.symbol_table import generate_symbolTable, print_symbolTable
 from compilation.ir import generate_ir, print_ir
 from compilation.optimize import optimize, print_optimizedIR
+from compilation.nasm import generate_asm, print_asm
 
 
 '''
@@ -28,6 +29,7 @@ def main():
     argparser.add_argument('-s', '--symbolTable', action='store_true')
     argparser.add_argument('-i', '--ir', action='store_true')
     argparser.add_argument('-o', '--optimize', action='store_true')
+    argparser.add_argument('-asm', '--assemblyCode', action='store_true')
 
     argparser.add_argument('filename')
     args = argparser.parse_args()
@@ -52,6 +54,9 @@ def main():
     if args.optimize:
         ir = optimize(ir, symbolTable)
         print_optimizedIR(ir)
+    if args.assemblyCode:
+        assembly_code = generate_asm(ir)
+        print_asm(assembly_code)
     # query_symbolTable('val2', symbolTable)
 
 
